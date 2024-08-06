58ac7d7a26f4ebe2252cbfeedfbb99cbb6ad354a
Frabosco <93011366+Frabosco@users.noreply.github.com>
2024-08-06 21:12:49 +0200

    minor fix

--- Easter2-main/src/config.py
+++ Easter2-main/src/config.py
@@ -3,7 +3,9 @@ Before training and evaluation - make sure to select desired/correct settings
 """
 
 # Input dataset related settings
-DATA_PATH = "IAM/"
+DATA_PATH = "Easter2-main/IAM/"
+DATA_PATH_V2= "Easter2-main/data/NoAugmentation"
+
 INPUT_HEIGHT = 80
 INPUT_WIDTH = 2000
 INPUT_SHAPE = (INPUT_WIDTH, INPUT_HEIGHT)
@@ -11,19 +13,20 @@ INPUT_SHAPE = (INPUT_WIDTH, INPUT_HEIGHT)
 TACO_AUGMENTAION_FRACTION = 0.9
 
 # If Long lines augmentation is needed (see paper)
-LONG_LINES = True
+
+LONG_LINES = False
 LONG_LINES_FRACTION = 0.3
 
 # Model training parameters
 BATCH_SIZE = 32
-EPOCHS = 100
+EPOCHS = 150
 VOCAB_SIZE = 80
 DROPOUT = True
 OUTPUT_SHAPE = 500
 
 # Initializing weights from pre-trained 
-LOAD = True
-LOAD_CHECKPOINT_PATH = "weights/saved_checkpoint.hdf5"
+LOAD = False
+LOAD_CHECKPOINT_PATH = "Easter2-main\weights\saved_checkpoint.hdf5"
 
 # Other learning parametes
 LEARNING_RATE = 0.01
@@ -31,6 +34,6 @@ BATCH_NORM_EPSILON = 1e-5
 BATCH_NORM_DECAY = 0.997
 
 # Checkpoints parametes
-CHECKPOINT_PATH = 'weights/EASTER2--{epoch:02d}--{loss:.02f}.keras'
-LOGS_DIR = 'logs'
-BEST_MODEL_PATH = "weights/saved_checkpoint.hdf5"
+CHECKPOINT_PATH = 'Easter2-main\weights/EASTER2--{epoch:02d}--{loss:.02f}.hdf5'
+LOGS_DIR = 'Easter2-main\logs'
+BEST_MODEL_PATH = "Easter2-main\weights/saved_checkpoint.hdf5"
